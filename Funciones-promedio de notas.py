6941d0eba63e40d49fab2d86dd4a5b6f1fd8d3ca
lozanocrismary111-png <lozanocrismary111@gmail.com>
2026-09-10 18:16:57 -0500

    Tarea semana 13
    Uso de funciones y operaciones para dar con el promedio del semestre de un estudiante

---
+++ Funciones-promedio de notas.py
@@ -0,0 +1,8 @@
+def promedio(parcial1, parcial2):
+    resultado = parcial1 + parcial2
+    resultado = resultado/2
+    return resultado
+
+if __name__ == "__main__":
+    llamada = promedio(10, 7.3)
+    print(llamada)
